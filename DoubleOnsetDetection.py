a3b437c939a0f683c7e74e539a54e3b3284f54e8
Snehesh Nag <33204313+SneheshNag@users.noreply.github.com>
2020-05-15 03:23:31 -0400

    Update DoubleOnsetDetection.py

--- DoubleOnsetDetection.py
+++ DoubleOnsetDetection.py
@@ -7,7 +7,6 @@ for frame in estd.FrameGenerator(audio, frameSize=1024, hopSize=512, startFromZe
   en.append(energy(frame))
 emax=max(en)
 ep=en.index(emax)
-print ep
 th=emax/2
 e=[]
 for frame in estd.FrameGenerator(audio[:0.6*44100], frameSize=1024, hopSize=512, startFromZero=True):
@@ -16,11 +15,6 @@ for i in range(0,len(e)-1):
   d=e[i+1]-e[i]
   if d > 0.01:
     p=e.index(e[i+1])
-    print p
     if (ep-p) > 2:
-      print "double onset = 1" 
       en=e[:p]+en[ep:]
       break
-print "double onset = %s" %(ep-p)
-print e[:35]
-print en[:35]
